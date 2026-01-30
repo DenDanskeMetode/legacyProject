43203647d2af6ed7edec023a5d8aa75657a87798
Christian-Skovgaard <cellochr@gmail.com>
2026-01-30 10:54:22 +0100

    works

--- app/main.go
+++ app/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("Server started 😨🎏")
 	http.HandleFunc("/test", testFunc)
-
+	log.Fatal(http.ListenAndServe(":8000", nil)) //start server
 }
 
 func testFunc(w http.ResponseWriter, r *http.Request) {
